dc9a4a1710052e151c68cd069df8c7360822d54d
Jantzen Ward <jward@degdigital.com>
2016-02-24 16:15:47 -0600

    Fixed cleaning carrier cache method to clean the cache in a manner that also works with unit tests.

--- app/code/community/DEG/Carriers/Helper/Data.php
+++ app/code/community/DEG/Carriers/Helper/Data.php
@@ -4,6 +4,6 @@ class DEG_Carriers_Helper_Data extends Mage_Core_Helper_Abstract
 {
     public function cleanCarrierCache()
     {
-        Mage::app()->getCacheInstance()->cleanType('config'); //Magento saves carrier data in core config
+        Mage::app()->getStore()->resetConfig(); //Magento saves carrier data in core config
     }
 }
